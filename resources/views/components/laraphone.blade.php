7f3cced92f1bdfa8979b7557b4ad11894f04ac3e
Imani Manyara <imani@simtabi.com>
2022-02-08 16:46:19 -0500

    Bug fixes

--- resources/views/components/laraphone.blade.php
+++ resources/views/components/laraphone.blade.php
@@ -1,38 +1,96 @@
-{{-- Hidden phone input --}}
-<input
-  type="hidden"
-  {{ $attributes->wire('model') }}
-  id="{{ $id }}"
-  name="{{ $name }}"
-  @if ($attributes->has('value'))
-    value="{{ $attributes->get('value') }}"
-  @endif
-  autocomplete="off"
->
 
-{{-- Tel input --}}
-<span wire:ignore>
-  <input
-    type="tel"
-    class="iti--laraphone {{ $attributes->get('class') }}"
-    data-phone-input-id="{{ $id }}"
-    data-phone-input-name="{{ $name }}"
-    data-phone-input="#{{ $id }}"
-    @if ($attributes->has('value'))
-      value="{{ $attributes->get('value') }}"
-    @endif
-    @if ($attributes->has('phone-country-input'))
-      data-phone-country-input="{{ $attributes->get('phone-country-input') }}"
-    @endif
-    @if ($attributes->has('placeholder'))
-      placeholder="{{ $attributes->get('placeholder') }}"
-    @endif
-    @if ($attributes->has('required'))
-      required
-    @endif
-    @if ($attributes->has('disabled'))
-    disabled
-    @endif
-    autocomplete="off"
-  >
-</span>
+<div wire:ignore>
+    @foreach($locales as $locale)
+        @php
+            $id                   = $getId($locale) ?: $getDefaultId($type, $locale);
+            $label                = $getLabel($locale);
+            $displayFloatingLabel = $shouldDisplayFloatingLabel();
+            $placeholder          = $getPlaceholder($label, $locale);
+            $value                = $getValue($locale);
+            $prepend              = $getPrepend($locale);
+            $append               = $getAppend($locale);
+            $errorMessage         = $getErrorMessage($errors, $locale);
+            $validationClass      = $getValidationClass($errors, $locale);
+            $isWired              = $componentIsWired();
+        @endphp
+
+        {{-- Hidden phone input --}}
+        <input
+            type="hidden" {{ $attributes->wire('model') }} id="{{ $id }}" name="{{ $name }}"
+            @if ($attributes->has('value'))
+            value="{{ $attributes->get('value') }}"
+            @endif
+            autocomplete="off"
+        >
+
+        {{-- Tel input --}}
+
+        <div @class(['form-floating' => $displayFloatingLabel, 'mb-3' => $marginBottom])>
+        @if(($prepend || $append) && ! $displayFloatingLabel)
+                <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+                <div class="input-group">
+                @endif
+                    @if(! $prepend && ! $append && ! $displayFloatingLabel)
+                        <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+                    @endif
+                    @if($prepend && ! $displayFloatingLabel)
+                        <x-gesanda::form-addon :addon="$prepend"/>
+                    @endif
+                <input {{ $attributes->except('wire')->merge([
+                'wire:model' . $getComponentLivewireModifier() => $isWired && ! $hasComponentNativeLivewireModelBinding() ? ($locale ? $name . '.' . $locale : $name) : null,
+                'id'                                           => $id,
+                'class'                                        => 'iti--laraphone form-control' . ($validationClass ? ' ' . $validationClass : null) . ' ' . $attributes->get('class'),
+                'type'                                         => $type,
+                'name'                                         => $isWired ? null : ($locale ? $name . '[' . $locale . ']' : $name),
+                'placeholder'                                  => $placeholder,
+                'data-locale'                                  => $locale,
+                'value'                                        => $isWired ? null : ($value ?? ''),
+                'aria-describedby'                             => $caption ? $id . '-caption' : null,
+            ]) }}
+
+                       data-phone-input-id="{{ $id }}"
+                       data-phone-input-name="{{ $name }}"
+                       data-phone-input="#{{ $id }}"
+
+                       @if ($attributes->has('value'))
+                       value="{{ $attributes->get('value') }}"
+                       @endif
+
+                       @if ($attributes->has('phone-country-input'))
+                       data-phone-country-input="{{ $attributes->get('phone-country-input') }}"
+                       @endif
+
+                       @if ($attributes->has('placeholder'))
+                       placeholder="{{ $attributes->get('placeholder') }}"
+                       @endif
+
+                       @if ($attributes->has('required'))
+                       required
+                       @endif
+
+                       @if ($attributes->has('disabled'))
+                       disabled
+                       @endif
+                       autocomplete="off"
+                />
+                @if(! $prepend && ! $append && $displayFloatingLabel)
+                        <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+                    @endif
+                    @if($append && ! $displayFloatingLabel)
+                        <x-gesanda::form-addon :addon="$append"/>
+                    @endif
+                <x-gesanda::form-caption :inputId="$id" :caption="$caption"/>
+                <x-gesanda::error :message="$errorMessage"/>
+                @if(($prepend || $append) && ! $displayFloatingLabel)
+            </div>
+            @endif
+    </div>
+    @endforeach
+</div>
+
+
+
+
+
+
+
--- src/Views/Components/TelInput.php
+++ src/Views/Components/TelInput.php
@@ -2,25 +2,55 @@
 
 namespace Simtabi\Laraphone\Views\Components;
 
+use Closure;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Simtabi\Gesanda\Abstracts\AbstractComponent;
+use Simtabi\Gesanda\Traits\Form\CanBeWired;
+use Simtabi\Gesanda\Traits\Form\HasAddon;
+use Simtabi\Gesanda\Traits\Form\HasFloatingLabel;
+use Simtabi\Gesanda\Traits\Form\HasId;
+use Simtabi\Gesanda\Traits\Form\HasLabel;
+use Simtabi\Gesanda\Traits\Form\HasName;
+use Simtabi\Gesanda\Traits\Form\HasPlaceholder;
+use Simtabi\Gesanda\Traits\Form\HasValidation;
+use Simtabi\Gesanda\Traits\Form\HasValue;
 
-class TelInput extends Component
+class TelInput extends AbstractComponent
 {
-    public $id;
-    public $name;
+    use HasId;
+    use HasName;
+    use HasLabel;
+    use HasFloatingLabel;
+    use HasValue;
+    use HasPlaceholder;
+    use HasAddon;
+    use HasValidation;
+    use CanBeWired;
 
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct(string $id = null, string $name = 'phone')
-    {
-        $this->id   = $id;
-        $this->name = $name;
+    /** @SuppressWarnings(PHPMD.ExcessiveParameterList) */
+    public function __construct(
+        public    string                        $name                     = 'phone',
+        protected string|null                   $id                       = null,
+        public    string                        $type                     = 'tel',
+        protected array|object|null             $bind                     = null,
+        protected string|false|null             $label                    = null,
+        protected bool|null                     $floatingLabel            = null,
+        protected string|false|null             $placeholder              = null,
+        protected string|Closure|null           $prepend                  = null,
+        protected string|Closure|null           $append                   = null,
+        protected string|int|array|Closure|null $value                    = null,
+        public    string|null                   $caption                  = null,
+        protected bool|null                     $displayValidationSuccess = null,
+        protected bool|null                     $displayValidationFailure = null,
+        protected string|null                   $errorBag                 = null,
+        public    array                         $locales                  = [null],
+        public    bool                          $marginBottom             = true
+    ) {
+
+        parent::__construct();
 
         if (!$this->name) {
             $this->name = 'phone-' . uniqid();
@@ -30,13 +60,11 @@ class TelInput extends Component
         }
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return Application|Factory|View
-     */
-    public function render()
+    protected function setViewPath(): string
     {
-        return view('laraphone::components.laraphone');
+        $this->asIs = true;
+        
+        return 'laraphone::components.laraphone';
     }
+
 }

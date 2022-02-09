34b536924940cd3aae8c830aa9f0aea937c4bc6c
Imani Manyara <imani@simtabi.com>
2022-02-09 15:28:14 -0500

    Bug fixes

--- resources/views/components/laraphone.blade.php
+++ resources/views/components/laraphone.blade.php
@@ -1,97 +1,94 @@
+@foreach($locales as $locale)
+    @php
+        $id                   = $getId($locale) ?: $getDefaultId($type, $locale);
+        $label                = $getLabel($locale);
+        $displayFloatingLabel = $shouldDisplayFloatingLabel();
+        $placeholder          = $getPlaceholder($label, $locale);
+        $value                = $getValue($locale);
+        $prepend              = $getPrepend($locale);
+        $append               = $getAppend($locale);
+        $errorMessage         = $getErrorMessage($errors, $locale);
+        $validationClass      = !empty($getValidationClass($errors, $locale)) ? $getValidationClass($errors, $locale) : '';
+        $isWired              = $componentIsWired();
+        $wiredKey             = !empty($getNestedWireKey()) ? $getNestedWireKey().'.'.$name : $name;
+    @endphp
 
-<div wire:ignore>
-    @foreach($locales as $locale)
-        @php
-            $id                   = $getId($locale) ?: $getDefaultId($type, $locale);
-            $label                = $getLabel($locale);
-            $displayFloatingLabel = $shouldDisplayFloatingLabel();
-            $placeholder          = $getPlaceholder($label, $locale);
-            $value                = $getValue($locale);
-            $prepend              = $getPrepend($locale);
-            $append               = $getAppend($locale);
-            $errorMessage         = $getErrorMessage($errors, $locale);
-            $validationClass      = $getValidationClass($errors, $locale);
-            $isWired              = $componentIsWired();
-            $wiredKey             = !empty($getNestedWireKey()) ? $getNestedWireKey().'.'.$name : $name;
-        @endphp
+    {{-- Hidden phone input --}}
+    <input
+        type="hidden" {{ $attributes->wire('model') }} id="{{ $id }}" name="{{ $name }}"
+        @if ($attributes->has('value'))
+        value="{{ $attributes->get('value') }}"
+        @endif
+        autocomplete="off"
+    >
 
-        {{-- Hidden phone input --}}
-        <input
-            type="hidden" {{ $attributes->wire('model') }} id="{{ $id }}" name="{{ $name }}"
-            @if ($attributes->has('value'))
-            value="{{ $attributes->get('value') }}"
-            @endif
-            autocomplete="off"
-        >
+    {{-- Tel input --}}
 
-        {{-- Tel input --}}
-
-        <div @class(['form-floating' => $displayFloatingLabel, 'mb-3' => $marginBottom])>
+    <div @class(['form-floating' => $displayFloatingLabel, 'mb-3' => $marginBottom])>
         @if(($prepend || $append) && ! $displayFloatingLabel)
-                <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
-                <div class="input-group">
+            <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+            <div class="input-group">
                 @endif
-                    @if(! $prepend && ! $append && ! $displayFloatingLabel)
-                        <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
-                    @endif
-                    @if($prepend && ! $displayFloatingLabel)
-                        <x-gesanda::form-addon :addon="$prepend"/>
-                    @endif
-                <input {{ $attributes->except('wire')->merge([
-                'wire:model' . $getComponentLivewireModifier() => $isWired && ! $hasComponentNativeLivewireModelBinding() ? ($locale ? $wiredKey . '.' . $locale : $wiredKey) : null,
-                'id'                                           => $id,
-                'class'                                        => 'iti--laraphone form-control' . ($validationClass ? ' ' . $validationClass : null) . ' ' . $attributes->get('class'),
-                'type'                                         => $type,
-                'name'                                         => $isWired ? null : ($locale ? $name . '[' . $locale . ']' : $name),
-                'placeholder'                                  => $placeholder,
-                'data-locale'                                  => $locale,
-                'value'                                        => $isWired ? null : ($value ?? ''),
-                'aria-describedby'                             => $caption ? $id . '-caption' : null,
-            ]) }}
+                @if(! $prepend && ! $append && ! $displayFloatingLabel)
+                    <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+                @endif
+                @if($prepend && ! $displayFloatingLabel)
+                    <x-gesanda::form-addon :addon="$prepend"/>
+                @endif
 
-                       data-phone-input-id="{{ $id }}"
-                       data-phone-input-name="{{ $name }}"
-                       data-phone-input="#{{ $id }}"
+                <div class="{{$validationClass}} wire-validation" >
+                    <div wire:ignore>
+                        <input {{ $attributes->except('wire')->merge([
+                        'wire:model' . $getComponentLivewireModifier() => $isWired && ! $hasComponentNativeLivewireModelBinding() ? ($locale ? $wiredKey . '.' . $locale : $wiredKey) : null,
+                        'id'                                           => $id,
+                        'class'                                        => 'iti--laraphone form-control ' .$validationClass. ' ' . $attributes->get('class'),
+                        'type'                                         => $type,
+                        'name'                                         => $isWired ? null : ($locale ? $name . '[' . $locale . ']' : $name),
+                        'placeholder'                                  => $placeholder,
+                        'data-locale'                                  => $locale,
+                        'value'                                        => $isWired ? null : ($value ?? ''),
+                        'aria-describedby'                             => $caption ? $id . '-caption' : null,
+                    ]) }}
 
-                       @if ($attributes->has('value'))
-                       value="{{ $attributes->get('value') }}"
-                       @endif
+                               data-phone-input-id="{{ $id }}"
+                               data-phone-input-name="{{ $name }}"
+                               data-phone-input="#{{ $id }}"
 
-                       @if ($attributes->has('phone-country-input'))
-                       data-phone-country-input="{{ $attributes->get('phone-country-input') }}"
-                       @endif
+                               @if ($attributes->has('value'))
+                               value="{{ $attributes->get('value') }}"
+                               @endif
 
-                       @if ($attributes->has('placeholder'))
-                       placeholder="{{ $attributes->get('placeholder') }}"
-                       @endif
+                               @if ($attributes->has('phone-country-input'))
+                               data-phone-country-input="{{ $attributes->get('phone-country-input') }}"
+                               @endif
 
-                       @if ($attributes->has('required'))
-                       required
-                       @endif
+                               @if ($attributes->has('placeholder'))
+                               placeholder="{{ $attributes->get('placeholder') }}"
+                               @endif
 
-                       @if ($attributes->has('disabled'))
-                       disabled
-                       @endif
-                       autocomplete="off"
-                />
+                               @if ($attributes->has('required'))
+                               required
+                               @endif
+
+                               @if ($attributes->has('disabled'))
+                               disabled
+                               @endif
+                               autocomplete="off"
+                        />
+                    </div>
+                </div>
                 @if(! $prepend && ! $append && $displayFloatingLabel)
-                        <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
-                    @endif
-                    @if($append && ! $displayFloatingLabel)
-                        <x-gesanda::form-addon :addon="$append"/>
-                    @endif
-                <x-gesanda::form-caption :inputId="$id" :caption="$caption"/>
+                    <x-gesanda::form-label :id="$id" class="form-label" :label="$label"/>
+                @endif
+                @if($append && ! $displayFloatingLabel)
+                    <x-gesanda::form-addon :addon="$append"/>
+                @endif
                 <x-gesanda::error :message="$errorMessage"/>
+                @if (empty($errorMessage))
+                    <x-gesanda::form-caption :inputId="$id" :caption="$caption"/>
+                @endif
                 @if(($prepend || $append) && ! $displayFloatingLabel)
             </div>
-            @endif
+        @endif
     </div>
-    @endforeach
-</div>
-
-
-
-
-
-
-
+@endforeach

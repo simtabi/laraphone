ea24262a73b1c1c42e50aeaf41b70f6517d4a781
Imani Manyara <imani@simtabi.com>
2022-02-13 14:42:06 -0500

    Bug fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/assets/js/laraphone.js', 'public/js')
+mix
+    .js('resources/assets/js/laraphone.js', 'public/js')
+    .js('resources/assets/js/intl-tel-input-utils.js', 'public/js')
     .postCss("resources/assets/css/laraphone.css", "public/css", [
         // require("tailwindcss"),
     ]);
